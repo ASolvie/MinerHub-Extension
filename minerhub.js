ca582510aa053924b79a244c5bb0c35cd7cd9f02
athakaspen <qwertydave54@gmail.com>
2022-04-20 16:12:11 -0500

    Vivaldi support and updating logos on interval

--- minerhub.js
+++ minerhub.js
@@ -1,18 +1,25 @@
+if (typeof browser === "undefined") {
+    var browser = chrome;
+}
+
 //document.body.style.border = "5px solid red";
 //console.log(browser.extension.getURL("images/minerHubNew.png"));
 var address = browser.extension.getURL("images/minerHubNew.png");
 //console.log(document.getElementsByClassName('topbar__icon-large'));
 
-try{
-    document.getElementsByClassName('hidden-xs hidden-sm hidden-md')[0].src = address;
-    document.getElementsByClassName('hidden-lg')[0].src = address;
-    document.getElementsByClassName('img-responsive mx-auto')[0].src = address;
-} catch(error){};
-try{
-    document.getElementsByClassName('topbar__icon-small')[0].src = address;
-    document.getElementsByClassName('topbar__icon-large')[0].src = address;
-    document.getElementsByClassName('topbar__icon-small')[0].style = "max-height: 74px; max-width: 90px; margin: 5px -20px;";
-} catch(error){};
-//
+function changeLogos(){
+    try{
+        document.getElementsByClassName('hidden-xs hidden-sm hidden-md')[0].src = address;
+        document.getElementsByClassName('img-responsive mx-auto')[0].src = address;
+        document.getElementsByClassName('hidden-lg')[0].src = address;
+    } catch(error){};
+    try{
+        document.getElementsByClassName('topbar__icon-small')[0].src = address;
+        document.getElementsByClassName('topbar__icon-large')[0].src = address;
+        document.getElementsByClassName('topbar__icon-small')[0].style = "max-height: 74px; max-width: 90px; margin: 5px -20px;";
+    } catch(error){};
+}
+
+setInterval(changeLogos, 200);
 
 console.log("working?");
