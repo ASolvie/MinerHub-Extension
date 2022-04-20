5322a822ea4c2b7561cdd8d12a6f210e52fa7cd0
ASolvie <austen@solvie.com>
2022-04-20 15:58:44 -0500

    Add all the things

---
+++ minerhub.js
@@ -0,0 +1,18 @@
+//document.body.style.border = "5px solid red";
+//console.log(browser.extension.getURL("images/minerHubNew.png"));
+var address = browser.extension.getURL("images/minerHubNew.png");
+//console.log(document.getElementsByClassName('topbar__icon-large'));
+
+try{
+    document.getElementsByClassName('hidden-xs hidden-sm hidden-md')[0].src = address;
+    document.getElementsByClassName('hidden-lg')[0].src = address;
+    document.getElementsByClassName('img-responsive mx-auto')[0].src = address;
+} catch(error){};
+try{
+    document.getElementsByClassName('topbar__icon-small')[0].src = address;
+    document.getElementsByClassName('topbar__icon-large')[0].src = address;
+    document.getElementsByClassName('topbar__icon-small')[0].style = "max-height: 74px; max-width: 90px; margin: 5px -20px;";
+} catch(error){};
+//
+
+console.log("working?");
